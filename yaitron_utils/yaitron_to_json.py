615676fc677fd5c5e2e7aeca2702bd06be752deb
Vee Satayamas <vsatayamas@gmail.com>
2016-05-23 22:04:55 +0700

    to json dict

---
+++ yaitron_utils/yaitron_to_json.py
@@ -0,0 +1,22 @@
+import sys
+import sqlite3
+from yaitron_reader import YaitronReader 
+import json
+
+reload(sys)        
+sys.setdefaultencoding('utf8')        
+
+reader = YaitronReader("../data/yaitron.xml")
+
+eng_dict = {}
+
+for entry in reader.read():
+  if entry.lang == 'en':
+    if entry.headword not in eng_dict:
+      eng_dict[entry.headword] = []
+    eng_dict[entry.headword].append(entry.pos)
+
+for k in eng_dict:
+  eng_dict[k] = list(set(eng_dict[k]))
+
+print json.dumps(eng_dict)
